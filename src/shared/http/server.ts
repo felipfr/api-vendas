e728880e3505d4eabc74170be97717ef1a45526f
Felipe Rodrigues <fdaldegan@gmail.com>
2023-05-23 11:36:12 -0300

    criação da rotas

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,20 @@
-console.log('Hello Worldsssssssssssssssssss');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+const hostname = 'localhost';
+const port = 3333;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, hostname, () => {
+  console.log(
+    '\x1b[33m%s\x1b[0m',
+    `Server running on http://${hostname}:${port} 🥳`,
+  );
+});
