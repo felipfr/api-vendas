daf31fcb4aaeb11831584f2ea947a13b2fa9cae4
Felipe Rodrigues <fdaldegan@gmail.com>
2023-05-23 12:25:09 -0300

    adicionado middleware para tratamento de erros

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -11,6 +12,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(port, hostname, () => {
   console.log(
