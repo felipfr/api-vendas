83fb71a47b9f2d6db8903a5cfea9e75fc6efa31f
Felipe Rodrigues <fdaldegan@gmail.com>
2023-06-01 16:47:36 -0300

    add controller de recuperação de senha

---
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -0,0 +1,15 @@
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+import { Request, Response } from 'express';
+
+export default class ForgotPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { email } = req.body;
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService();
+
+    await sendForgotPasswordEmail.execute({
+      email,
+    });
+
+    return res.status(204);
+  }
+}
