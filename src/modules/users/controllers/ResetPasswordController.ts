46cf36b25246baba9c74af55bab5db3763c50cd9
Felipe Rodrigues <fdaldegan@gmail.com>
2023-06-01 17:09:39 -0300

    add controller de reset de senha

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,16 @@
+import ResetPasswordService from '../services/ResetPasswordService';
+import { Request, Response } from 'express';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { password, token } = req.body;
+    const resetPassword = new ResetPasswordService();
+
+    await resetPassword.execute({
+      password,
+      token,
+    });
+
+    return res.sendStatus(204);
+  }
+}
