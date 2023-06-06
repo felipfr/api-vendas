92b3bfa192da05555079f0e9460d710f687fb23e
Felipe Rodrigues <fdaldegan@gmail.com>
2023-06-06 16:27:37 -0300

    add config de email e alterações no .env

---
+++ src/config/mail/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: process.env.MAIL_ADDRESS || 'contato@vendas.com',
+      name: process.env.MAIL_USERNAME || 'Contato',
+    },
+  },
+} as IMailConfig;
