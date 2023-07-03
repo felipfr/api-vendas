a2381c09753d77917bb454bd1b9cc907834361f2
Felipe Rodrigues <me@felipefr.dev>
2023-07-02 21:57:32 -0300

    refatoração do módulo de 'Orders'

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -1,46 +1,48 @@
 import AppError from '@shared/infra/http/errors/AppError';
-import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
-import Order from '../infra/typeorm/entities/Order';
-import OrdersRepository from '../infra/typeorm/repositories/OrdersRepository';
-import ProductRepository from '@modules/products/infra/typeorm/repositories/ProductsRepository';
-import { getCustomRepository } from 'typeorm';
+import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepository';
+import { IOrder } from '../domain/models/IOrder';
+import { IOrdersRepository } from '../domain/repositories/IOrdersRepository';
+import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
+import { IRequestCreateOrder } from '../domain/models/IRequestCreateOrder';
+import { inject, injectable } from 'tsyringe';
 
-interface IProduct {
-  id: string;
-  quantity: number;
-}
-
-interface IRequest {
-  customer_id: string;
-  products: IProduct[];
-}
-
+@injectable()
 class CreateOrderService {
-  public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    const ordersRepository = getCustomRepository(OrdersRepository);
-    const customersRepository = getCustomRepository(CustomersRepository);
-    const productsRepository = getCustomRepository(ProductRepository);
-    const customerExists = await customersRepository.findById(customer_id);
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+
+    @inject('ProductRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
+
+  public async execute({
+    customer_id,
+    products,
+  }: IRequestCreateOrder): Promise<IOrder> {
+    const customerExists = await this.customersRepository.findById(customer_id);
 
     if (!customerExists) {
       throw new AppError(`Couldn't find any customer with the given id`);
     }
 
-    const existsProducts = await productsRepository.findAllByIds(products);
+    const existsProducts = await this.productsRepository.findAllByIds(products);
 
     if (!existsProducts.length) {
-      throw new AppError(`Couldn't find any products with the given id's`);
+      throw new AppError(`Couldn't find any customer with the given id`);
     }
 
     const existsProductsIds = existsProducts.map(product => product.id);
-
     const checkInexistentProducts = products.filter(
       product => !existsProductsIds.includes(product.id),
     );
 
     if (checkInexistentProducts.length) {
       throw new AppError(
-        `Couldn't find product '${checkInexistentProducts[0].id}'`,
+        `Couldn't find product ${checkInexistentProducts[0].id}.`,
       );
     }
 
@@ -52,7 +54,8 @@ class CreateOrderService {
 
     if (quantityAvailable.length) {
       throw new AppError(
-        `The quantity '${quantityAvailable[0].quantity}' isn't available for '${quantityAvailable[0].id}'`,
+        `The quantity ${quantityAvailable[0].quantity}
+        isn't available for ${quantityAvailable[0].id}.`,
       );
     }
 
@@ -62,21 +65,21 @@ class CreateOrderService {
       price: existsProducts.filter(p => p.id === product.id)[0].price,
     }));
 
-    const order = await ordersRepository.createOrder({
+    const order = await this.ordersRepository.create({
       customer: customerExists,
       products: serializedProducts,
     });
 
     const { order_products } = order;
 
-    const updatedProductQuantity = order_products.map(product => ({
+    const updatedProductsQuantity = order_products.map(product => ({
       id: product.product_id,
       quantity:
         existsProducts.filter(p => p.id === product.product_id)[0].quantity -
         product.quantity,
     }));
 
-    await productsRepository.save(updatedProductQuantity);
+    await this.productsRepository.updateStock(updatedProductsQuantity);
 
     return order;
   }
--- src/modules/orders/services/ShowOrderService.ts
+++ src/modules/orders/services/ShowOrderService.ts
@@ -1,23 +1,23 @@
 import AppError from '@shared/infra/http/errors/AppError';
-import Order from '../infra/typeorm/entities/Order';
-import OrdersRepository from '../infra/typeorm/repositories/OrdersRepository';
-import { getCustomRepository } from 'typeorm';
+import { IOrder } from '../domain/models/IOrder';
+import { IOrdersRepository } from '../domain/repositories/IOrdersRepository';
+import { IShowOrder } from '../domain/models/IShowOrder';
+import { inject, injectable } from 'tsyringe';
 
-interface IRequest {
-  id: string;
-}
-
+@injectable()
 class ShowOrderService {
-  public async execute({ id }: IRequest): Promise<Order> {
-    const ordersRepository = getCustomRepository(OrdersRepository);
-    const order = await ordersRepository.findById(id);
+  constructor(
+    @inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository,
+  ) {}
+
+  public async execute({ id }: IShowOrder): Promise<IOrder> {
+    const order = await this.ordersRepository.findById(id);
 
     if (!order) {
-      throw new AppError('Order not found');
+      throw new AppError('Order not found.');
     }
-
     return order;
   }
 }
-
 export default ShowOrderService;
